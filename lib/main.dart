b239cf3151f00fbe8c1dbc6edba87f3fe48a1be6
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2019-01-11 21:56:03 +0900

    code base

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Top Lakes'),
         ),
         body: Center(
           child: Text('Hello World'),
